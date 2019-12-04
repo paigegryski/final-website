be8538c887344b56fd09de509f22d7bd640617b0
Paige <paigegryski@gmail.com>
2019-12-04 17:49:57 -0500

    smooth scroll

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,3 +1,162 @@
+<style type="text.css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  /* Home */
+  .home {
+    background-image: url("../images/home-bkgnd.jpg");
+    background-repeat: no-repeat;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    image-resolution: 1024px;
+    text-align: center;
+    font-size: 78px;
+    font-family: "PT Serif";
+    padding: 250px;
+  }
+  /* Text Styling */
+  h1 {
+    font-size: 45px;
+    font-family: "PT Serif";
+    color: black;
+    letter-spacing: normal;
+    width: 1000px;
+    padding: 30px 0 30px 50px;
+  }
+
+  h2 {
+    padding: 0 0 0 250px;
+    font-family: "PT Sans";
+  }
+
+  h3 {
+    padding: 0 0 0 280px;
+  }
+
+  p {
+    font-family: "Lato";
+  }
+  /* Navigation Sidebar */
+  nav ul li {
+    display: block;
+  }
+
+  .navigation {
+    background: white;
+    line-height: 40px;
+    border-radius: 15px;
+    border-color: #d9d9d9;
+    border-style: solid;
+    border-width: thick;
+    position: fixed;
+    color: black;
+    padding: 10px 20px;
+    text-align: center;
+    right: 50px;
+    top: 200px;
+    margin: auto;
+    font-family: "PT Serif";
+    text-shadow: 1px 1px lightgray;
+  }
+
+  a:link {
+    color:black;
+    text-decoration: none;
+  }
+  a:visited {
+    color:black;
+    text-decoration: none;
+    font-family: "Lato";
+  }
+
+  a:hover {
+    color: darkgray;
+  }
+
+  /* About */
+  .paige {
+    image-resolution: 1024px;
+    height: 300px;
+    padding: 10px 10px 30px 200px;
+    float: left;
+  }
+
+  .about {
+    background-color: #fed3ce;
+    display: inline-block;
+    padding: 0 0 50px 0;
+  }
+  /* Resume */
+  .resume {
+    background-image: url("../images/resume-bkgnd.jpg");
+    padding: 0 0 50px 50px;
+    line-height: 30px;
+  }
+  /* Contact */
+  .facebook, .instagram, .twitter, .linkedin{
+    width: 70px;
+    padding: 60px 100px 60px 300px;
+  }
+
+  /* Image Hover */
+  .facebook:hover {
+  width: 75px;
+  }
+
+  .instagram:hover {
+  width: 75px;
+  }
+
+  .twitter:hover {
+  width: 75px;
+  }
+
+  .linkedin:hover {
+  width: 75px;
+  }
+
+  .contact {
+    background-image: url("../images/contact-bkgnd.jpg");
+    background-repeat: no-repeat;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    image-resolution: 1024px;
+    padding: 20px 0 20px 0;
+  }
+
+  .contactform {
+      padding: 60px 100px 60px 300px;
+  }
+
+  .test_form {
+    max-width: 500px;
+    border-radius: 10px;
+    color: black;
+    padding: 0 0 0 50px;
+    margin: 40px auto;
+    font-family: "Lato";
+    display: block;
+  }
+
+  .submit input {
+    background: white;
+    color: black;
+    padding: 5px 20px;
+    border-radius: 4px;
+    font-family: "PT Sans";
+  }
+
+
+  p {
+    padding: 0 200px 0 300px;
+  }
+</style>
   <header>
     <nav>
       <ul>
