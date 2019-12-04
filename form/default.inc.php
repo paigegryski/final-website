486ce50eab1de605d3f38de805424073f14e3210
Paige <paigegryski@gmail.com>
2019-12-03 22:38:47 -0500

    more of contact form
    need to fix styling for text box

---
+++ form/default.inc.php
@@ -0,0 +1,54 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+nav ul li {
+  display: inline;
+}
+
+header {
+  text-align: center;
+  background: #d9d9d9;
+  padding: 20px;
+}
+
+nav a {
+  text-decoration: none;
+  color: black;
+  background: #fed3ce;
+  padding: 5px;
+  border-radius: 4px;
+}
+
+.contact-form {
+  background: #fed3ce;
+  padding: 20px;
+  max-width: 500px;
+  border-radius: 10px;
+  color: black;
+  font-family: sans-serif;
+  margin: 40px auto;
+}
+
+footer {
+  background:#d9d9d9;
+  color: black;
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
+
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="container">
+    <h1>Sorry, something went wrong with your submission.</h1>
+    <p>Please try again.</p>
+  </div>
